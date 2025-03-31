34fafb629bf11763bb45db8769d858e4f4d2e41b
Nicolas Bassini <nicolasbassini@hotmail.com>
2025-03-30 21:02:44 -0300

    new: ordering microservice project start

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("Database") ?? "";
+        return services;
+    }
+}
